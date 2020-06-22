8c3041ec3edc9748f8314171d9fa1386ee9053b8
MIchael Irwin <pickledmisfit@gmail.com>
2020-06-22 12:26:18 -0500

    Initial Commit

---
+++ MichaelCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MichaelCard
+//
+//  Created by Michael Irwin on 2020/22/02.
+//  Copyright © 2020 Pickled Developers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
